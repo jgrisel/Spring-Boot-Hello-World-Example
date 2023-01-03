f051423dc8fdf9d66901c1df9bac7e136f3ad71f
jgrisel <101192270+jgrisel@users.noreply.github.com>
2023-01-03 16:20:36 +0100

    Update DemoApplicationTests.java

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -16,7 +16,7 @@ class DemoApplicationTests {
         void groupedAssertions() {
         Personne pers = new Personne("Nassur", "Moumadi");
 
-        assertAll("personne", () -> assertEquals("Nassure", pers.getFirstName()),
+        assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
                               () -> assertEquals("Moumadi", pers.getLastName()));
     }
 }
