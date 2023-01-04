f7fc6d9f4668f7b4bbd08a5a0c4ff89de52314c8
jgrisel <101192270+jgrisel@users.noreply.github.com>
2023-01-04 10:05:24 +0100

    Update HomeController.java

--- src/main/java/com/reytech/demo/controller/HomeController.java
+++ src/main/java/com/reytech/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String hello() {
+    public String hello(){
         return "hello";
     }
 
